a25c5b5570fa1205fbd9fd8d2c9679745ff84c95
MICA_ADMIN <webmaster@micatechnologies.com>
2020-01-20 13:29:34 -0500

    Better image renders

--- Textures/eps2img.sh
+++ Textures/eps2img.sh
@@ -1,5 +1,5 @@
 set +x
 echo NOW CONVERTING ALL EPS IN CURRENT FOLDER TO PNG
 find . -type f -iname "*.png" -delete
-find . -type f -iname "*.eps" -exec convert -trim +repage {} {}.png \;
+find . -type f -iname "*.eps" -exec convert -density 300 -trim -resize "256x256!" +repage {} {}.png \;
 set -x
